4c9b942ab1205e485803dfb5c269b17490d7f96c
Mazekaz <zanym1102@gmail.com>
2021-04-02 18:54:32 +0700

    Upgrade Check Http&Https

--- PingDomainStatus_V2.py
+++ PingDomainStatus_V2.py
@@ -21,6 +21,11 @@ def FilterDomain(DomainFile):
     domainUnique = set(listDomain)
     return domainUnique
 
+def DomainFunc(url):
+    r = requests.get(url, verify=False)
+    DomainStatus = r.status_code
+    return DomainStatus
+
 def DomainStatusCheck():
     # open results file and write header
     try:
@@ -34,30 +39,32 @@ def DomainStatusCheck():
         print(ioerr)
 
     urllib3.disable_warnings()
-    i = 1
+    i = 0
     for domain in FilterDomain(DomainFile):
+        i += 1
         try:
             if "http" not in domain:
-                url = "https://"+domain
-            else:
-                url = domain
-
-            r = requests.get(url, verify=False)
-            DomainStatus = r.status_code
-
-            # Comment Domain
-            if DomainStatus >= 100 and DomainStatus < 200:
-                Comment = "Informational Response – the request was received, continuing process"
-            elif DomainStatus >= 200 and DomainStatus < 300:
-                Comment = "Successful – the request was successfully received, understood, and accepted"
-            elif DomainStatus >= 300 and DomainStatus < 400:
-                Comment = "Redirection – further action needs to be taken in order to complete the request"
-            elif DomainStatus >= 400 and DomainStatus < 500:
-                Comment = "Client Error – the request contains bad syntax or cannot be fulfilled"
-            elif DomainStatus >= 500 and DomainStatus < 600:
-                Comment = "Server Error – the server failed to fulfil an apparently valid request"
-            else:
-                Comment = "Unknown Error"
+                url = "http://"+domain
+                DomainStatus = DomainFunc(url)
+                if DomainStatus == 200:
+                    Comment = "Successful – the request was successfully received, understood, and accepted"
+                else:
+                    url = "https://"+domain
+                    DomainStatus = DomainFunc(url)
+                    if DomainStatus == 200:
+                        Comment = "Successful – the request was successfully received, understood, and accepted"
+                    else: 
+                        # Comment Domain
+                        if DomainStatus >= 100 and DomainStatus < 200:
+                            Comment = "Informational Response – the request was received, continuing process"
+                        elif DomainStatus >= 300 and DomainStatus < 400:
+                            Comment = "Redirection – further action needs to be taken in order to complete the request"
+                        elif DomainStatus >= 400 and DomainStatus < 500:
+                            Comment = "Client Error – the request contains bad syntax or cannot be fulfilled"
+                        elif DomainStatus >= 500 and DomainStatus < 600:
+                            Comment = "Server Error – the server failed to fulfil an apparently valid request"
+                        else:
+                            Comment = "Unknown Error"
 
             print("Checking {0}: {1} => {2}".format(i, url, DomainStatus))
 
@@ -68,10 +75,11 @@ def DomainStatusCheck():
             # yield data
             dataWriter.writerow(data)
 
-            i += 1
         except requests.exceptions.ConnectionError:
             continue
         except KeyboardInterrupt:
             break
 
 DomainStatusCheck()
+
+
