53876f8501823528197fd628e27a0b8ec74c0f91
Mazekaz <zanym1102@gmail.com>
2021-04-02 16:27:10 +0700

    Add Unique Domain

--- PingDomainStatus_V2.py
+++ PingDomainStatus_V2.py
@@ -6,80 +6,72 @@ import urllib3
 
 # ==> Enter File Domain <==
 DomainFile = "DomainRikkei/DomainAllinOne.txt"
-chunkSize = 3
-
-def DomainStatusCheck(DomainFile):
-    data = []
-    for domain in FilterDomain(DomainFile):
-        try:
-            domain = domain.strip('\n')
-            print(domain)
-            # """
-            nowDateTime = datetime.now().strftime("%D %H:%M:%S")
-            if 'http' not in domain:
-                prefixDomain = 'http://'
-                domainCheck = prefixDomain+domain
-                DomainStatus = Domain_StatusCode(domainCheck)
-                if DomainStatus == 200:
-                    data.append([nowDateTime, domainCheck, DomainStatus])
-                else:
-                    prefixDomain = 'https://'
-                    domainCheck = prefixDomain+domain
-                    DomainStatus = Domain_StatusCode(domainCheck)
-                    if DomainStatus == 200:
-                        data.append([nowDateTime, domainCheck, DomainStatus])
-                    else:
-                        prefixDomain = 'http://www.'
-                        domainCheck = prefixDomain+domain
-                        DomainStatus = Domain_StatusCode(domainCheck)
-                        if DomainStatus == 200:
-                            data.append([nowDateTime, domainCheck, DomainStatus])
-                        else:
-                            prefixDomain = 'https://www.'
-                            domainCheck = prefixDomain+domain
-                            DomainStatus = Domain_StatusCode(domainCheck)
-                            if DomainStatus == 200:
-                                data.append([nowDateTime, domainCheck, DomainStatus])
-                            else:
-                                data.append([nowDateTime, domainCheck, DomainStatus])
-            # """
-        except ConnectionError:
-            print(ConnectionError)
-
-    return data
-
-def Domain_StatusCode(domainCheck):
-    http = urllib3.PoolManager()
-    r = http.request('GET', domainCheck)
-    DomainStatus = r.status
-    print(DomainStatus)
-    return DomainStatus
 
 def FilterDomain(DomainFile):
     regex = re.compile(r"(^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?)$|([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5})")
+    listDomain = []
     with open(DomainFile, 'r') as infile:
         for domain in infile:
             domain = domain.strip('\n')
             result = regex.match(domain)
             if result != None:
                 dataDomain = result.group()
-                # dataDomain = set(result.group())
-                yield dataDomain
+                listDomain.append(dataDomain)
 
-def main(DomainFile, chunkSize):
+    domainUnique = set(listDomain)
+    return domainUnique
+
+def DomainStatusCheck():
     # open results file and write header
     try:
         rfile = open('results.csv', 'w+', newline='')
         dataWriter = csv.writer(rfile, delimiter = ',')
-        header = ['Scan Date', 'Domain', 'Status']
+        header = ['Scan Date', 'Domain', 'Status', 'Comment']
         dataWriter.writerow(header)
 
     except IOError as ioerr:
         print('Please ensure the file is closed.')
         print(ioerr)
 
-    ResultDataDomain = DomainStatusCheck(DomainFile)
-    for data in ResultDataDomain:
-        dataWriter.writerow(data)
+    urllib3.disable_warnings()
+    i = 1
+    for domain in FilterDomain(DomainFile):
+        try:
+            if "http" not in domain:
+                url = "https://"+domain
+            else:
+                url = domain
 
-main(DomainFile, chunkSize)
+            r = requests.get(url, verify=False)
+            DomainStatus = r.status_code
+
+            # Comment Domain
+            if DomainStatus >= 100 and DomainStatus < 200:
+                Comment = "Informational Response – the request was received, continuing process"
+            elif DomainStatus >= 200 and DomainStatus < 300:
+                Comment = "Successful – the request was successfully received, understood, and accepted"
+            elif DomainStatus >= 300 and DomainStatus < 400:
+                Comment = "Redirection – further action needs to be taken in order to complete the request"
+            elif DomainStatus >= 400 and DomainStatus < 500:
+                Comment = "Client Error – the request contains bad syntax or cannot be fulfilled"
+            elif DomainStatus >= 500 and DomainStatus < 600:
+                Comment = "Server Error – the server failed to fulfil an apparently valid request"
+            else:
+                Comment = "Unknown Error"
+
+            print("Checking {0}: {1} => {2}".format(i, url, DomainStatus))
+
+            nowDateTime = datetime.now().strftime("%D %H:%M:%S")
+
+            data = [nowDateTime, url, DomainStatus, Comment]
+
+            # yield data
+            dataWriter.writerow(data)
+
+            i += 1
+        except requests.exceptions.ConnectionError:
+            continue
+        except KeyboardInterrupt:
+            break
+
+DomainStatusCheck()
